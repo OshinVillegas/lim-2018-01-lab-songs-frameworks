5bb08ef92df614f1f77edf0a5b7d3d43ae05269d
unknown <villegasoshin25@gmail.com>
2018-10-23 16:38:57 -0500

    creando component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { MusicaComponent } from './components/musica/musica.component';
+import { NavComponent } from './components/nav/nav.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MusicaComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule
